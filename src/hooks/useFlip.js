7c11c979e4d7c2e5b0047ff17ed767d989009636
Engines-screaming <karl.ravago@gmail.com>
2022-08-20 18:27:20 -0700

    refactor pokemon card and playing card flip actions with custom hook called useFlip

---
+++ src/hooks/useFlip.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+/* hook to handle state of flipping cards */
+function useFlip() {
+    const [isFacingUp, setIsFacingUp] = useState(true);
+    const flipCard = () => {
+        setIsFacingUp(isUp => !isUp);
+    };
+    return [isFacingUp, flipCard];
+}
+
+export default useFlip;
+
